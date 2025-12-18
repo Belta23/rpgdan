1f861e7a3a0da257b52a3dff7605a68feef4ff5e
Daniel <danielferrreira@outlook.com>
2025-12-18 13:28:01 -0300

    Ajustes no player.php e correções de layout

--- player.php
+++ player.php
@@ -1,8 +1,8 @@
 <?php
-// ================================
-// CONFIGURAÇÃO DE BANCO
-// ================================
-// Reaproveite seu env.php existente
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once __DIR__ . '/env.php';
 
 
