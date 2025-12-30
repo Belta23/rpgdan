978d0c388165d3fae5c1b9c2507f0b378ba9e16c
Daniel <danielferrreira@outlook.com>
2025-12-30 14:01:10 -0300

    1ª lista de cards

--- cards.php
+++ cards.php
@@ -1,1 +1,15 @@
-não há nada aqui!
+        <ul>
+          <li>A primeira batalha</li>
+          <li>O primeiro amigo</li>
+          <li>A batalha na arena</li>
+          <li>O Tribunal</li>
+          <li>A traição</li>
+          <li>O recomeço</li>
+         <li>O cerco d e Curkarn</li>
+          <li>Um novo aliado</li>
+          <li>Um aliado duvidoso</li>
+          <li>O coliseu</li>
+          <li>Marx e Karl</li>
+          <li>Um inimigo Sujo</li>
+          <li>A espionagem</li>
+        </ul>
